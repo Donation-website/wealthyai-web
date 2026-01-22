72dccddb2b2386f43bf69a74c116145aed83f326
Donation-website <lipcseitundetimea@gmail.com>
2026-01-22 07:35:53 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ export default function Home() {
       style={{
         minHeight: "100vh",
         display: "flex",
-        flexDirection: "column", // Oszlop elrendezés, hogy az ikonok alulra kerülhessenek
+        flexDirection: "column",
         alignItems: "center",
         justifyContent: "center",
         backgroundImage: "linear-gradient(rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.6)), url('/wealthyai/wealthyai.png')",
@@ -12,11 +12,9 @@ export default function Home() {
         backgroundPosition: 'center',
         backgroundRepeat: 'no-repeat',
         color: "white",
-        fontFamily: "Arial, sans-serif",
-        position: "relative" // Hogy az ikonokat pontosan elhelyezhessük
+        fontFamily: "Arial, sans-serif"
       }}
     >
-      {/* Középső szöveg */}
       <div style={{ textAlign: "center", flex: 1, display: "flex", flexDirection: "column", justifyContent: "center" }}>
         <h1 style={{ fontSize: "3rem", marginBottom: "0.5rem" }}>WealthyAI</h1>
         <p style={{ opacity: 0.9, fontSize: "1.2rem" }}>
@@ -24,21 +22,21 @@ export default function Home() {
         </p>
       </div>
 
-      {/* Ikonok a lap alján */}
       <div style={{ 
         display: "flex", 
         gap: "20px", 
         paddingBottom: "40px" 
       }}>
         <a href="https://facebook.com" target="_blank" rel="noopener noreferrer">
-          <img src="/wealthyai/icons/fb.png" alt="Facebook" style={{ width: "32px", height: "32px", cursor: "pointer" }} />
+          <img src="/wealthyai/icons/fb.png" alt="Facebook" style={{ width: "32px", height: "32px" }} />
         </a>
         <a href="https://instagram.com" target="_blank" rel="noopener noreferrer">
-          <img src="/wealthyai/icons/insta.png" alt="Instagram" style={{ width: "32px", height: "32px", cursor: "pointer" }} />
+          <img src="/wealthyai/icons/insta.png" alt="Instagram" style={{ width: "32px", height: "32px" }} />
         </a>
         <a href="https://x.com" target="_blank" rel="noopener noreferrer">
-          <img src="/wealthyai/icons/x.png" alt="X" style={{ width: "32px", height: "32px", cursor: "pointer" }} />
+          <img src="/wealthyai/icons/x.png" alt="X" style={{ width: "32px", height: "32px" }} />
         </a>
       </div>
     </main>
   );
+}
