0d3e663280b214465daedc292de3d3e4255d702d
Donation-website <lipcseitundetimea@gmail.com>
2026-01-22 08:19:37 +0100

    Update create-checkout-session.js

--- pages/api/create-checkout-session.js
+++ pages/api/create-checkout-session.js
@@ -1,15 +1,16 @@
 // pages/api/create-checkout-session.js
 import Stripe from 'stripe';
 
+// A titkos kulcsot a Vercel Environment Variables-ből húzza be
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
 export default async function handler(req, res) {
   if (req.method === 'POST') {
     const { priceId } = req.body;
     try {
-      // Helyettesítsd be a majdani éles domain neveddel:
-      const successUrl = `${req.headers.origin}/success?session_id={CHECKOUT_SESSION_ID}`;
-      const cancelUrl = `${req.headers.origin}/?cancelled=true`;
+      // Itt add meg a majdani sikeres és sikertelen visszatérési URL-eket
+      const successUrl = `${req.headers.origin}/success`; // Egyelőre csak a főoldalra visz vissza
+      const cancelUrl = `${req.headers.origin}/`; 
 
       const session = await stripe.checkout.sessions.create({
         line_items: [
@@ -23,6 +24,7 @@ export default async function handler(req, res) {
         cancel_url: cancelUrl,
       });
 
+      // Visszaküldi a kliensnek a Stripe session ID-t
       res.status(200).json({ sessionId: session.id });
     } catch (err) {
       res.status(500).json({ error: 'Error creating checkout session' });
