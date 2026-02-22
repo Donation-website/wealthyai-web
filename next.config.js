7c95498ebfc2a4eebbe1dde39b84a057f509eb7e
Donation-website <lipcseitundetimea@gmail.com>
2026-02-22 20:35:28 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,26 +1,22 @@
-const withObfuscator = require('nextjs-obfuscator');
+const JavaScriptObfuscator = require('webpack-obfuscator');
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: false
+  reactStrictMode: false,
+  webpack: (config, { isServer }) => {
+    if (!isServer && process.env.NODE_ENV === 'production') {
+      config.plugins.push(
+        new JavaScriptObfuscator({
+          compact: true,
+          controlFlowFlattening: false,
+          stringArray: true,
+          stringArrayThreshold: 0.75,
+          unicodeEscapeSequence: true,
+        }, [])
+      );
+    }
+    return config;
+  },
 }
 
-module.exports = withObfuscator(nextConfig, {
-  obfuscateFiles: {
-    buildManifest: true,
-    ssg: true,
-    ssr: true,
-    chunks: true,
-    pages: true,
-  },
-  obfuscationOptions: {
-    compact: true,
-    controlFlowFlattening: false,
-    deadCodeInjection: false,
-    debugProtection: false,
-    disableConsoleOutput: false,
-    stringArray: true,
-    stringArrayThreshold: 0.75,
-    unicodeEscapeSequence: true,
-  }
-});
+module.exports = nextConfig;
