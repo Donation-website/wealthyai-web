19b1f4e191ef2de927b2f4ba1037a10ba20d526b
Donation-website <lipcseitundetimea@gmail.com>
2026-02-22 20:32:05 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,18 +1,26 @@
-const withJavaScriptObfuscator = require('next-javascript-obfuscator');
+const withObfuscator = require('nextjs-obfuscator');
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false
 }
 
-module.exports = withJavaScriptObfuscator(nextConfig, {
-  compact: true,
-  controlFlowFlattening: false,
-  deadCodeInjection: false,
-  debugProtection: false,
-  disableConsoleOutput: false,
-  stringArray: true,
-  stringArrayThreshold: 0.75,
-  unicodeEscapeSequence: true,
-  reservedStrings: ['stripe', 'supabase', 'google'],
+module.exports = withObfuscator(nextConfig, {
+  obfuscateFiles: {
+    buildManifest: true,
+    ssg: true,
+    ssr: true,
+    chunks: true,
+    pages: true,
+  },
+  obfuscationOptions: {
+    compact: true,
+    controlFlowFlattening: false,
+    deadCodeInjection: false,
+    debugProtection: false,
+    disableConsoleOutput: false,
+    stringArray: true,
+    stringArrayThreshold: 0.75,
+    unicodeEscapeSequence: true,
+  }
 });
