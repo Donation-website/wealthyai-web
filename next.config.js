cc1e7e3af14f7d379a5b3aa0ef3f7decb88603f2
Donation-website <lipcseitundetimea@gmail.com>
2026-03-13 13:15:06 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,18 +1,11 @@
+const JavaScriptObfuscator = require('webpack-obfuscator');
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
-  // EZ KELL A CLOUDFLARE-NEK:
-  experimental: {
-    runtime: 'edge',
-  },
   webpack: (config, { isServer }) => {
     if (!isServer && process.env.NODE_ENV === 'production') {
-      // Dinamikusan töltjük be, hogy ne akadjon meg a build az elején
-      const JavaScriptObfuscator = require('javascript-obfuscator');
-      const WebpackObfuscator = require('webpack-obfuscator');
-      
       config.plugins.push(
-        new WebpackObfuscator({
+        new JavaScriptObfuscator({
           compact: true,
           controlFlowFlattening: false,
           stringArray: true,
@@ -24,5 +17,4 @@ const nextConfig = {
     return config;
   },
 }
-
 module.exports = nextConfig;
